52403d16049118646797ed44157f611ff0b9b9c6
Alexandro Aguilar <auza920205@gmail.com>
2019-02-08 23:34:48 -0600

    privacy policy update and terms and conditions re-located

--- controllers/terms-conditions/index.js
+++ controllers/terms-conditions/index.js
@@ -4,7 +4,7 @@ var path = require('path');
 module.exports = function (router) {
 
   const options = {
-    root: path.join(__dirname, '../../public'),
+    root: path.join(__dirname, '../../public/privacy_policy'),
   }
 
   router.get('/', function (req, res) {
