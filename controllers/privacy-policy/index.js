1c1a578ecc4fd5eb8737fd3344b76ac4727edf8e
Alexandro Aguilar <auza920205@gmail.com>
2019-02-08 23:38:39 -0600

    privacy policy route fixed

---
+++ controllers/privacy-policy/index.js
@@ -0,0 +1,24 @@
+'use strict';
+var path = require('path');
+
+module.exports = function (router) {
+
+  const options = {
+    root: path.join(__dirname, '../../public/privacy_policy'),
+  }
+
+  router.get('/', function (req, res) {
+
+    res.sendFile('index.html', options, err => {
+      console.log(global.__basedir);
+      if(err)
+        console.log(err);
+      else{
+        console.log('Sent file');
+      }
+    });
+    //res.send('<p>Terms and conditions</p>');
+      
+  });
+
+};
